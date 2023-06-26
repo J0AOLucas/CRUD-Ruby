4428773c7a2ba9ae660fa5aa4807a197c0a148ec
JoaoLucas <lucasrpg.silva@gmail.com>
2023-06-26 15:15:15 -0300

    Last Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :products
-  resources :documentos
+  #resources :documentos
  
   resources :products do
     collection do
       post :import
     end
-    end
+  end
+  root 'products#index'
 end
